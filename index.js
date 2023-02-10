7344bcc0749efba7626d6e8fa3f713c1692ec1c7
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-02-10 18:46:16 -0500

    feat:changes of index.js

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ numbers.calculus.adaptiveSimpson(Math.sin, -2, 4, .0001);
 var myfunc = function(x) {
     return 2*Math.pow(x,2) + 1;
 }
+
+numbsers.calculus.Riemann(myfunc, -2, 4, 200);
+numbers.calculus.Riemann(myfunc, -2, 4, .0001);
