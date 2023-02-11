e79ebad77782e6d30df81c5eb099d6464bf63499
SofíaMG <98538133+FOOSSIILL@users.noreply.github.com>
2023-02-10 19:17:35 -0500

    feat: changes index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+///Liberia numbers
 var numbers = require('numbers');
 
 numbers.calculus.Riemann(Math.sin, -2, 4, 200);
@@ -10,3 +11,21 @@ var myfunc = function(x) {
 
 numbsers.calculus.Riemann(myfunc, -2, 4, 200);
 numbers.calculus.Riemann(myfunc, -2, 4, .0001);
+
+//Libreria Book
+
+var log = require('book');
+
+log.info('haters be loggin');
+
+log.info('hello world');
+ 
+log.info('secret: %d', 42);
+ 
+log.info('json: %j', { secret: 42 });
+
+//options 
+
+var log = require('book').default ({
+    stdout: false
+})
